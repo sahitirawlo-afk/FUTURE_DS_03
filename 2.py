eb5b0a31846c63f5ba2a1c521a47915fc222de8e
sahit <sahitirawlo@gmail.com>
2026-06-19 15:35:05 +0530

    Initial Commit

--- 2.py
+++
@@ -1,8 +0,0 @@
-# Step 1: Create the input file
-with open("input4.2.txt","w") as f:
-    f.write("Hello World\nPython Programming\nEEE Department")
-
-# Step 2: Read the file and reverse each line
-with open("input4.2.txt","r") as f:
-    for line in f:
-        print(line.strip()[::-1])
